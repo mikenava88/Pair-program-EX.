ad3d17550323f9efe93cb81f74ce02e7211b8f17
mikenava88 <mikenava88.mn@gmail.com>
2022-08-24 14:42:44 -0500

    1

---
+++ index.js
@@ -0,0 +1,22 @@
+/* declare the counter and queryselector for button
+/*if cookie button is clicked it should add 1 to i*/
+
+
+
+
+
+const cookiebutton = document.querySelector(".cookiebutton");
+const h1 = document.querySelector(".num")
+
+
+let i = 0;
+
+cookiebutton.addEventListener('click', function counter() {
+
+    i += 1;
+    h1.innerText = i;
+});
+
+
+
+
